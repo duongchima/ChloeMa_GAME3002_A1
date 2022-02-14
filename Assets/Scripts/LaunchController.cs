4ff8bb7567c3f0320d83cffc3e3dbc4ce00156d0
chi <ma.duongchi@gmail.com>
2022-02-14 03:00:37 -0500

    created game space

--- Assets/Scripts/LaunchController.cs
+++ Assets/Scripts/LaunchController.cs
@@ -4,33 +4,35 @@ using UnityEngine;
 
 public class LaunchController : MonoBehaviour
 {
-    [SerializeField] private Vector3 rotation;
     public float launchPower = 5f;
-    public float timerTotal = 3f;
-    float currentTime = 0f;
+    public float rotationSpeed = 5f;
     public GameObject projectiles;
     public Transform shotPoint;
-    float x;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     private void Update()
     {
-        currentTime += Time.deltaTime;
-        if (currentTime >= timerTotal)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             LaunchShoot();
-            currentTime -= timerTotal;
         }
-        transform.Rotate(rotation * Time.deltaTime);
     }
     void LaunchShoot()
     {
         GameObject firedProjectile = Instantiate(projectiles, shotPoint.position, shotPoint.rotation);
         firedProjectile.GetComponent<Rigidbody>().velocity = shotPoint.transform.up * launchPower;
+    }
+  void OnMouseDrag()
+    {
+        float XaxisRotation = Input.GetAxis("Mouse X") * rotationSpeed;
+        float YaxisRotation = Input.GetAxis("Mouse Y") * rotationSpeed;
+        transform.Rotate(Vector3.down, XaxisRotation);
+        transform.Rotate(Vector3.right, YaxisRotation);
     }
 }
